9cd739c2e3662bc3f1bef9a2526e716955f73bc9
kraftSM <kraftsm@yandex.ru>
2023-01-24 08:21:31 +0300

    Hw9_1-2 Add exception class + начало работ над Hw2

---
+++ caHw_9-2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caHw_9_2
+{
+    internal class Program
+    {
+        static string UnitName = "Un:9";
+        static string UnDescr = "Exeptiions/Action.";
+        static string ExName = "HW-2/2";
+        static string ExDescr = "Action/delegates";
+
+        static string[] dimOfFamily; 
+        static protected string UnTitle
+        {
+            get
+            { return UnitName + "[" + UnDescr + "]"; }
+        }
+        static protected string ExTitle
+        {
+            get
+            { return UnitName + "." + ExName + "[" + ExDescr + "]"; }
+        }
+        static protected string Promt
+        {
+            get
+            { return UnitName + "." + ExName + ":"; }
+        }
+        static void iniSortingDim()
+        {
+            dimOfFamily = new string[5];
+            dimOfFamily[0] = "Петров";
+            dimOfFamily[1] = "Брыльская"; 
+            dimOfFamily[2] = "Яковлев";
+            dimOfFamily[3] = "Рязанов";
+            dimOfFamily[4] = "Мягков"; 
+        }
+        static void ShowSortingDim()
+        {
+            for (int j = 0; j < dimOfFamily.Length; j++) Console.WriteLine("{0}: dimOfFamily[j] = {1} ", ExTitle, dimOfFamily[j]);
+            
+        }
+
+            static void Main(string[] args)
+        {
+            Console.WriteLine("{0}:Startig ", ExTitle);
+            Console.WriteLine("{0} ", Promt);
+            iniSortingDim();
+            ShowSortingDim();
+            try
+            {
+               
+                //    for (int j = 0; j < dimOfFamily.Length; j++)
+                //{ Console.WriteLine("{0}: dimOfFamily[j] = {1} ", ExTitle, dimOfFamily[j]);
+                //}
+                //if (TestPath(dPath))
+                //{
+                
+                //    //TestDir(dPath);
+                //}
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0}: Get Exception.", Promt, ex.Message);
+            }
+            Console.WriteLine("{0}: Finishing.", ExTitle);
+            Console.WriteLine("{0}: Finished. Press key.", UnTitle);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ caUn8_Hw4_4/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask
+{
+    [Serializable]
+    class Stud
+    {
+        //private string sName;
+        //private string sGrp;
+        //private DateTime sDOB;
+        public string StName { get; set; }
+        public string stGroup { get; set; }
+        public DateTime stDateOfBirth { get; set; }
+        //public Stud(string Name, string Group, DateTime BurthDate)
+        //{
+        //    sName = Name;
+        //    sGrp = Group;
+        //    sDOB = BurthDate;
+        //}
+
+    }
+    internal class Program
+    {
+        static string inFileNametlPath = "Students.dat";
+        static string inFileFullNamelPath = @"D:\WinTemp\SkillFactory\Students.dat";
+        static string inFilePath = "D:\\WinTemp\\SkillFactory\\";
+        static void Main(string[] args)
+        {
+            ReadBinary();
+
+            Console.WriteLine("NfskEnd");
+            Console.ReadLine();
+        }
+        static void ReadBinary()
+        {
+            // string sName;
+            //string sGrp;
+            //DateTime sDOB;
+
+            if (File.Exists(inFileFullNamelPath))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                // десериализация
+                using (FileStream fs = new FileStream(inFileFullNamelPath, FileMode.Open))
+                {
+                    Stud[] rdStud = (Stud[])bf.Deserialize(fs);
+                    Console.WriteLine("Объект десериализован");
+                    //foreach (Stud st in rdStud)
+                    //{
+                    //    Console.WriteLine($"Имя: {st.StName} --- gr: {st.stGroup} lh: {st.stDateOfBirth}");
+                    //}
+                }
+            }
+
+            //try { 
+            //if (File.Exists(inFileFullNamelPath))
+            //{
+            //        var newPet = (Pet)formatter.Deserialize(fs);
+            //        Console.WriteLine("Объект десериализован");
+            //        Console.WriteLine($"Имя: {newPet.Name} --- Возраст: {newPet.Age}"); 
+            //        using (var stream = File.Open(inFileFullNamelPath, FileMode.Open))
+            //    {
+            //        using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
+            //        {
+            //            sName = reader.ReadString();
+            //            sGrp = reader.ReadString();
+            //            sDOB = Convert.ToDateTime( reader.ReadInt32());
+
+            //        }
+            //    }
+
+            //    Console.WriteLine("sName is: " + sName);
+            //    Console.WriteLine("sGrp  is: " + sGrp);
+            //    Console.WriteLine("sDOB is: "+ sDOB);
+            //    }          
+            //}
+
+        }
+    }
+}
